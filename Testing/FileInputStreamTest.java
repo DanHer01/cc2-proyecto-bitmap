85bc040467ce4fd6e328e883aa069c9b3baa7dbf
PestoChiliPizza <johnkramer0101@gmail.com>
2022-09-18 16:04:17 -0600

    Added more tests to FileInputStream. Managed to generate first copy from original image.

--- Testing/FileInputStreamTest.java
+++ Testing/FileInputStreamTest.java
@@ -2,29 +2,45 @@ import java.io.*;
 import java.util.Arrays;
 
 
-public class FileInputStreamTest  {
+public class FileInputStreamTest  {  
+    
     public static void main(String[] args) throws IOException {
-        int[] listOfBytes = new int[54];
-        InputStream fis = new FileInputStream("Testing\\tokyoRetro.bmp");  
-        for (int i = 0; i < 54; i++) {
+        InputStream fis = new FileInputStream("Testing\\tokyoRetro.bmp"); 
+        OutputStream fos = new FileOutputStream("Testing\\newImageTest.bmp");
+        OutputStream redImage = new FileOutputStream("Testing\\RedImage.bmp");
+        
+        //Getting the length of FileInputStream to initialize array
+        int lengthOriginal = fis.available();
+        
+        //This is where we save and modify the array
+        int[] listOfBytes = new int[lengthOriginal];
+        int[] redArray = new int[lengthOriginal];
+        
+        for (int i = 0; i < lengthOriginal; i++) {
             listOfBytes[i] = fis.read(); 
+            fos.write(listOfBytes[i]);
         }
-        System.out.println(Arrays.toString(listOfBytes));
-        // Image FileSize in a String of bits
-        String BitCount = Integer.toBinaryString(listOfBytes[29]) + Integer.toBinaryString(listOfBytes[30]);
-        System.out.println(BitCount + " value in bits");
-        //int foo = Integer.parseInt(BitCount, 2);
-        //System.out.println(foo);
+        //Print Array of pixels (Optional)
+        //System.out.println(Arrays.toString(listOfBytes));
         
+        //Test reading BM
+        //char c = (char) (b & 0xFF);
+        char c = (char) (listOfBytes[0]);
+        char c2 = (char) (listOfBytes[1]);
+        System.out.println(c +"\s"+ c2);
         
+        //Second image
+        for (int i = 0; i < 54; i++) {
+            redArray[i] = listOfBytes[i]; 
+            redImage.write(redArray[i]);
+        }
+        for (int i = 54; i < lengthOriginal; i=i+1) {
+            redArray[i] = listOfBytes[i]; 
+            redImage.write(redArray[i]);
+        }
+        System.out.println(Arrays.toString(redArray)); 
 
-        //Display first to bytes as characters
-        
-        //Reading filesize
-        File f = new File("Testing\\tokyoRetro.bmp");
-        long fileSize = f.length();
-        
-        System.out.format("The size of the file: %d bytes", fileSize);
-        
     }
+
+    
 }
